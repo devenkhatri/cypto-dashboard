cc19d8e2b21cd28f47db637ab5857a8aa5e4cd53
devenkhatri <deven.khatri@gmail.com>
2021-08-16 07:21:56 +0000

    tailwindcss added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.<div className="text-blue-700 text-sm"> - Tailwinded</div>
         </p>
         <a
           className="App-link"
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  // darkMode: media, // or 'media' or 'class'
+  theme: {
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
