bf891fdd286510b0cc437907fb0fa7c948e3ce2c
devenkhatri <deven.khatri@gmail.com>
2021-08-16 07:54:38 +0000

    ui created

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="grid grid-cols-1 gap-6 place-content-center h-56 w-full p-8 text-center">          
-          <div className="text-5xl font-bold text-primary">$ 2000.00</div>
-          <div className="text-sm text-secondary">Last updated: 2 mins ago</div>
+          <div className="text-7xl font-bold text-primary">$ 2000.00</div>
+          <div className="text-xl text-secondary">Last updated: 2 mins ago</div>
         </div>
       </header>
     </div>
