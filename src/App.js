73961a8d52694040cef2876dd424bbe7c54a599a
devenkhatri <deven.khatri@gmail.com>
2021-08-16 08:00:07 +0000

    fixed colors

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="grid grid-cols-1 gap-6 place-content-center h-56 w-full p-8 text-center">          
-          <div className="text-7xl font-bold text-primary">$ 2000.00</div>
-          <div className="text-xl text-secondary">Last updated: 2 mins ago</div>
+      <header className="App-header h-72 w-full">
+        <div className="grid grid-cols-1 gap-6 place-content-center p-8 text-center">          
+          <div className="text-7xl font-bold text-green-300">$ 2000.00</div>
+          <div className="text-xl text-yellow-200">Last updated: 2 mins ago</div>
         </div>
       </header>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
-const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   // darkMode: media, // or 'media' or 'class'
   theme: {
-    colors: {
-      primary: colors.indigo,
-      secondary: colors.yellow,
-      neutral: colors.gray,
-    },
     extend: {},
   },
   variants: {
